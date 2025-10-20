18663ca3f10061ac6617a32884e0e3fc5ed1aa76
samuel <saa2280@cumc.columbia.edu>
2025-10-20 10:53:37 +0300

    This update modifies the unstuck schedule stored procedure to improve data integrity and fix stuck rows.
    ** The procedure now copies the start time from the last known successful schedule and applies it to the stuck row. This prevents data loss that could occur between the time of the last successful schedule and when the unstuck procedure is executed.
    
    ** Additionally, this change extends the same unstuck logic to handle schedule outcomes that resulted in an error, ensuring those rows are also corrected properly.

--- api/src/main/resources/_core/database/mysql/xf_system/etl_scheduler/sp_mamba_etl_un_stuck_scheduler.sql
+++ api/src/main/resources/_core/database/mysql/xf_system/etl_scheduler/sp_mamba_etl_un_stuck_scheduler.sql
@@ -8,7 +8,7 @@ BEGIN
     DECLARE running_schedule_record BOOLEAN DEFAULT FALSE;
     DECLARE no_running_mamba_sp BOOLEAN DEFAULT FALSE;
     DECLARE last_schedule_record_id INT;
-    DECLARE end_time DATETIME DEFAULT DATE_ADD(NOW(),INTERVAL -5 YEAR );
+    DECLARE incremental_start_time DATETIME DEFAULT DATE_ADD(NOW(),INTERVAL -5 YEAR );
     DECLARE error_schedule BOOLEAN DEFAULT FALSE;
 
     SET last_schedule_record_id = (SELECT MAX(id) FROM _mamba_etl_schedule limit 1);
@@ -28,9 +28,9 @@ BEGIN
                                           FROM performance_schema.events_statements_current
                                           WHERE SQL_TEXT LIKE 'CALL sp_mamba_etl_scheduler_wrapper(%'
                                              OR SQL_TEXT = 'CALL sp_mamba_etl_scheduler_wrapper()');
-    SET end_time = (SELECT start_time
+    SET incremental_start_time = (SELECT start_time
                     FROM _mamba_etl_schedule sch
-                    WHERE end_time IS NOT NULL
+                    WHERE incremental_start_time IS NOT NULL
                       AND transaction_status = 'COMPLETED'
                       AND completion_status = 'SUCCESS'
                       AND success_or_error_message is null
@@ -39,14 +39,16 @@ BEGIN
 
     IF running_schedule_record AND no_running_mamba_sp AND NOT error_schedule THEN
     UPDATE _mamba_etl_schedule
-    SET end_time                 = end_time,
+    SET end_time                 = incremental_start_time,
+        start_time               = incremental_start_time,
         completion_status        = 'SUCCESS',
         transaction_status       = 'COMPLETED',
         success_or_error_message = 'Stuck schedule updated'
     WHERE id = last_schedule_record_id;
     ELSEIF error_schedule THEN
     UPDATE _mamba_etl_schedule
-    SET end_time                 = end_time,
+    SET end_time                 = incremental_start_time,
+        start_time               = incremental_start_time,
         completion_status        = 'SUCCESS',
         transaction_status       = 'COMPLETED',
         success_or_error_message = 'Error schedule updated'
